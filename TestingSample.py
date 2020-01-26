ea67bb6a5e3a6cfaf118a1e9c5a3a1f3969ac8c9
Roopak2020 <roopak.voleti73@gmail.com>
2020-01-26 15:22:48 +0530

    updated

--- TestingSample.py
+++ TestingSample.py
@@ -1,5 +1,3 @@
-#Pushing on to user's branch
-print("This is jenkins job created!!!")
-print("Jenkins Job is Running")
-print("Pipeline added")
-print("This is printed by user2:Shivangi singh")
+# This is the updated code onto branch
+
+print("This is updated code by reviewer")

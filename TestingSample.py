1523a72806be9543abe5eabbbf313ad18c25a082
Roopak2020 <roopak.voleti73@gmail.com>
2020-01-26 15:20:07 +0530

    Developer's code

--- TestingSample.py
+++ TestingSample.py
@@ -1,5 +1,4 @@
-#Pushing on to user's branch
-print("This is jenkins job created!!!")
-print("Jenkins Job is Running")
-print("Pipeline added")
-print("This is printed by user2:Shivangi singh")
+# This is POC
+
+Print("This is developers account")
+

9e53fc594ff7fbfe37971128a27c936193e83902
sisingh <ankisingh4200@gmail.com>
2020-01-27 17:07:46 +0530

    This is edited by shivangi

--- README.md
+++
@@ -1,2 +0,0 @@
-# ITSec_OPS_Code 
-This repo is for ITSecurity_Operations
--- Sample.py
+++ Sample.py
@@ -3,3 +3,4 @@ print("This code is written by Shivangi[Dev2]")
 #This part is from dev2
 print("This is developer 2 code")
 print("--------------------------------------")
+print("this is the code")
